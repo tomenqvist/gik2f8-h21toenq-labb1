068063b5ba86977bd3883f12d5303a295a0332a1
tomenqvist <90455414+tomenqvist@users.noreply.github.com>
2022-12-11 14:45:51 +0100

    styling

--- script.js
+++ script.js
@@ -18,16 +18,7 @@ searchField.addEventListener('keyup', (e) =>
   )
 );
 
-
-const divstart = `<div class="absolute bg-green-400 drop-shadow-xl" 
-                      id="bookDetails"
-                      style="max-width: 30vw;">`;
-                
-
-const divend = `</div>`
-let bookImg = `<img class='coverImg' src="`
 //Hover event
-
 
 document.getElementById("searchField").addEventListener("mouseout", function(){
   document.getElementById("searchField").style.backgroundColor = "white";
@@ -56,9 +47,10 @@ function renderBookList(bookList) {
 //renderar diven vid muspekaren när den hovrar över varje item
 function renderBookDetails(item){
   
-  item.addEventListener("mouseover", function(e){
+  item.addEventListener("mouseenter", function(e){
     var target = e.target;
-
+    console.log(target);
+    target.style.backgroundColor = "lavender";
     //Letar efter author och title i listelemenet och splittar dem
     let details = target.innerHTML.split(" - ");
     const objectAuthor = details[0].trim();
@@ -68,59 +60,62 @@ function renderBookDetails(item){
     //kollar om author och titel stämmer överens, om de gör de hämta bild och text baserat på id
     for (let index = 0; index < bookList.length; index++){
       if (bookList[index].author == objectAuthor && bookList[index].title == objectTitle){
-
-        console.log("före getBooks");
         let bookItem = getBook(index + 1).then((bookID) => {
-          let bookDetailsHtml = `<img class='coverImg' src="`;
-          bookDetailsHtml += bookID.coverImage;
-          bookDetailsHtml += `" alt="" srcset=""></img>`;
-          bookDetailsHtml += `<p>`;
+          // let divHTML = BookDetails(bookItem);
+          let bookHtmlID = `bookDetails${bookID.id}`
+          const divstart = `<div class="books absolute bg-gradient-to-tr from-purple-200 to-indigo-200 drop-shadow-xl" 
+                      id="${bookHtmlID}"
+                      style="max-width: 200px">`;
+                
+          
+          console.log(bookHtmlID);
+          // Bygger upp strängen för diven
+          let bookDetailsHtml = `<img src="`;
+          let image;
+          if(bookID.coverImage === "" ){
+            image = "/img/no_cover.svg";
+          } else {
+            image = bookID.coverImage
+          }
+          bookDetailsHtml += image;
+          bookDetailsHtml += `" alt="" srcset=""></img>
+                              <p>`;
           bookDetailsHtml += bookID.title;
-          bookDetailsHtml += `</p>`;
-          bookDetailsHtml += `<p>`;
+          bookDetailsHtml += `</p>
+                              <p>`;
           bookDetailsHtml += bookID.author;
           bookDetailsHtml += `</p>`;
+          const divend = `</div>`
           let divHTML = divstart + bookDetailsHtml + divend;
-          root.insertAdjacentHTML('beforeend', divHTML);
-          console.log(bookImg)
-
+          
+          
+          e.target.insertAdjacentHTML('beforeend', divHTML);
+          
           // Bestämmer vart diven ska renderas
-          let div = document.getElementById('bookDetails');
+          let divElement = document.getElementById(bookHtmlID);
+          
           let left = e.pageX + 25;
           let top = e.pageY + 25;
-          div.style.left = left + 'px';
-          div.style.top = top + 'px';
-        });
+          divElement.style.left = left + 'px';
+          divElement.style.top = top + 'px';
+          
+        }); //getBook funtion all end        
+      } // if end
+    } // for loop end
+  }); //eventlistener mouseenter end
+  
+  // Tar bort bookdetails
+  item.addEventListener("mouseout", function(e){
+    let target = e.target;
+    target.style.backgroundColor = "white";
+    console.log(document.getElementsByClassName('books')[0]);
+    // kör en loop och tar bort alla utifall buggen där en bok dröjer kvar (funkar
+    // inte jättbra)
+    for(let idx = 0; idx < bookList.length; idx++){
+      let bookDetailsDiv = document.getElementsByClassName('books')[idx]
+      if(bookDetailsDiv){
+        bookDetailsDiv.remove();
       }
     }
-    
-    
-
-    
-
-  })
-  // Tar bort bookDetails när muspekaren lämnar list itemet 
-  item.addEventListener("mouseout", function(){
-    document.getElementById("bookDetails").remove();
-  })
-}
-
-//hämtar boken från api:t baserat på id
-async function getBook(bookId){
-  const result = await fetch(url+`/${bookId}`).then((result) => result.json());
-
-
-  return result;
-}
-
-
-//test
-//console.log(getBook(1));
-
-
-
-
-
-
-
-
+  });// event listener mouseout end
+} // renderBookDetails end
